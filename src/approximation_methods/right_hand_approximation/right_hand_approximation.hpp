b63128e6448c3a8fc2750f4c7ce951fefb522d19
MarikTik <mtik.philosopher@gmail.com>
2024-03-04 17:27:22 -0800

    changed right hand rule approximation method to be a class method

--- src/approximation_methods/right_hand_approximation/right_hand_approximation.hpp
+++ src/approximation_methods/right_hand_approximation/right_hand_approximation.hpp
@@ -1,22 +1,25 @@
 #pragma once
 #include <cassert>
 #include <type_traits>
+#include <functional>
 
 // example code: 
 // note that the signatures will be changed at later stages, this is just an implementation example
 
 namespace integration::methods{
-     template<typename Function, typename Rational, typename Integer>
-     Rational right_hand_approximation(Function f, Rational a, Rational b, Integer n){
-        static_assert(std::is_invocable_v<Function, Rational> and std::is_integral_v<Integer>);
-        assert(n > 0); // n must be positive
-
-        Rational delta_x = (b - a) / n;
-        Rational sum = 0;
-        for (Rational i = 1; i <= n; i++){
-            sum += f(a + i * delta_x);
+    struct RightHandRuleApproximation
+    {
+        template<typename Function, typename Real, typename Integer>
+        Real approximate(Function f, Real a, Real b, Integer n){
+            static_assert(std::is_integral_v<Integer>);
+            assert(n > 0); // n must be positive
+            Real delta_x = (b - a) / n;
+            Real sum = 0;
+            for (Real i = 1; i <= n; i++){
+                sum += f(a + i * delta_x);
+            }
+            return delta_x * sum;
         }
-        return delta_x * sum;
-     }
+    };
 }
 
