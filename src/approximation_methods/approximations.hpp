3105d59258c8a45d13cf65477210f7edaaf1d99d
MarikTik <mtik.philosopher@gmail.com>
2024-03-04 17:18:36 -0800

    added approximation.hpp header that includes all approximation methods

---
+++ src/approximation_methods/approximations.hpp
@@ -0,0 +1,5 @@
+#include "left_hand_approximation/left_hand_approximation.hpp"
+#include "right_hand_approximation/right_hand_approximation.hpp"
+#include "trapezoidal_approximation/trapezoidal_approximation.hpp"
+#include "simpsons_approximation/simpsons_approximation.hpp"
+#include "mid_point_approximation/mid_point_approximation.hpp"
