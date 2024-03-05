d33202850ca21a2861714aa1666d27bebccddfc1
MarikTik <mtik.philosopher@gmail.com>
2024-03-04 17:33:14 -0800

    rewritten all approximation methods as classes
    for easier use with generic type arguments

--- src/approximation_methods/simpsons_approximation/simpsons_approximation.hpp
+++ src/approximation_methods/simpsons_approximation/simpsons_approximation.hpp
@@ -3,23 +3,26 @@
 #include <type_traits>
 
 namespace integration::methods{
-     template<typename Function, typename Rational, typename Integer>
-     Rational simpsons_approximation(Function f, Rational a, Rational b, Integer n){
-        static_assert(std::is_invocable_v<Function, Rational> and std::is_integral_v<Integer>);
-        assert(n > 0 and n % 2 not_eq 1); // n must be positive
-     
-        Rational delta_x = (b - a) / n;
-        Rational first_sum = 0;
-        Rational second_sum = 0;
+     struct SimpsonsApproximation{
+          template<typename Function, typename Rational, typename Integer>
+          Rational approximate(Function f, Rational a, Rational b, Integer n){
+             static_assert(std::is_invocable_v<Function, Rational> and std::is_integral_v<Integer>);
+             assert(n > 0 and n % 2 not_eq 1); // n must be positive
 
-        static auto x = [](Integer i){
-          return a + i * delta_x;
-        }
-        
-        for (Integer i = 1; i < n; i += 2) first_sum += f(x(i));
-        for (Integer i = 2; i < n; i += 2) second_sum += f(x(i));
-         
-        return delta_x / 3 * ((a) + (4 * first_sum) + (2 * second_sum) + f(b));
-     }
+             Rational delta_x = (b - a) / n;
+             Rational first_sum = 0;
+             Rational second_sum = 0;
+
+             static auto x = [](Integer i){
+               return a + i * delta_x;
+             }
+
+             for (Integer i = 1; i < n; i += 2) first_sum += f(x(i));
+             for (Integer i = 2; i < n; i += 2) second_sum += f(x(i));
+
+             return delta_x / 3 * ((a) + (4 * first_sum) + (2 * second_sum) + f(b));
+          }
+     };
+     
 }
 
--- src/approximation_methods/trapezoidal_approximation/trapezoidal_approximation.hpp
+++ src/approximation_methods/trapezoidal_approximation/trapezoidal_approximation.hpp
@@ -3,21 +3,24 @@
 #include <type_traits>
 
 namespace integration::methods{
-     template<typename Function, typename Rational, typename Integer>
-     Rational trapezoidal_approximation(Function f, Rational a, Rational b, Integer n){
-        static_assert(std::is_invocable_v<Function, Rational> and std::is_integral_v<Integer>);
-        assert(n > 0); // n must be positive
-     
-        Rational delta_x = (b - a) / n;
-        Rational sum = 0;
+     struct TrapezoidalApproximation{
+          template<typename Function, typename Rational, typename Integer>
+          Rational approximate(Function f, Rational a, Rational b, Integer n){
+             static_assert(std::is_invocable_v<Function, Rational> and std::is_integral_v<Integer>);
+             assert(n > 0); // n must be positive
 
-        static auto x = [](Integer i){
-          return a + i * delta_x;
-        }
-        
-        for (Integer i = 1; i < n; i +=1) sum += f(x(i));
-       
-        return delta_x * (0.5 * (f(a) + f(b)) + sum); 
-     }
+             Rational delta_x = (b - a) / n;
+             Rational sum = 0;
+
+             static auto x = [](Integer i){
+               return a + i * delta_x;
+             }
+
+             for (Integer i = 1; i < n; i +=1) sum += f(x(i));
+
+             return delta_x * (0.5 * (f(a) + f(b)) + sum); 
+          }
+     };
+     
 }
 
